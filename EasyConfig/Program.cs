6eccba3f6345b8cc0823841ca14f2db9798caf63
keven1229 <keven@kevenidc.com>
2021-01-01 16:55:57 +0800

    AutoUpdater

--- EasyConfig/Program.cs
+++ EasyConfig/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using FSLib.App.SimpleUpdater;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,43 @@ namespace EasyConfig
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            bool isAppRunning = false;
+            Mutex mutex = new Mutex(true, System.Diagnostics.Process.GetCurrentProcess().ProcessName, out isAppRunning);
+            if (!isAppRunning)
+            {
+                MessageBox.Show("程序已在运行，请检查任务栏！");
+                Environment.Exit(1);
+            }
+            var updater = Updater.CreateUpdaterInstance();
+            updater.Error += (s, e) =>
+            {
+                MessageBox.Show("更新发生了错误：" + updater.Context.Exception.Message, "自动更新");
+            };
+            updater.NoUpdatesFound += (s, e) =>
+            {
+                MessageBox.Show("当前版本已为最新！", "自动更新");
+            };
+            updater.MinmumVersionRequired += (s, e) =>
+            {
+                MessageBox.Show("当前版本过低无法使用自动更新！", "自动更新");
+            };
+            Updater.CheckUpdateSimple();
+
+            DateTime expire = DateTime.Parse("2021-12-14");
+            if (!updater.Context.HasUpdate)
+            {
+                if (expire < DateTime.Now)
+                {
+                    MessageBox.Show("版本已过期，即将关闭");
+                }
+                else
+                {
+                    Control.CheckForIllegalCrossThreadCalls = false;
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
+                }
+            }
         }
     }
 }
